579fb44c7c034445dbe3cdd6183aac59089760e2
Shreeja Princy <148578262+ShreejaPrincy@users.noreply.github.com>
2026-06-28 20:27:57 +0530

    Create 235. Lowest Common Ancestor of a Binary Search Tree.cpp

---
+++ 235. Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -0,0 +1,24 @@
+/*
+Time Complexity: O(h)
+
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while(root != NULL) {
+            if(p->val < root->val && q->val < root->val) {
+                root = root->left;
+            }
+            else if(p->val > root->val && q->val > root->val) {
+                root = root->right;
+            }
+            else {
+                return root;
+            }
+        }
+
+        return NULL;
+    }
+};
